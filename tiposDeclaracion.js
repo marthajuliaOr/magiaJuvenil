8431a7c8e2de9092f28c2f950d9f5857af13c862
Martha Julia <al02954101@tecmilenio.mx>
2021-04-05 23:13:12 -0700

    Subiendo mi primer archivo a un repositorio

---
+++ tiposDeclaracion.js
@@ -0,0 +1,78 @@
+
+//Programa número 1 en JavaScript
+
+//Declaración de Variable
+var número = 365;
+console.log( número );
+
+//Cambio de valor de Variable
+número = 5000;
+console.log( número );
+
+//declaración de Constante
+const mensaje = "Bienvenido a Ciudad Río Verde";
+//mensaje = "Bienvenido a Ciudad Río Claro";
+console.log( mensaje );
+
+//Características de persona
+const nombre = "Diego";
+const appelidoPaterno = "Perez";
+const appelidoMaterno = "Dominguez";
+var edad = 15;
+const lugarNacimiento = "México";
+
+console.log( nombre );
+console.log( appelidoPaterno );
+console.log( appelidoMaterno );
+console.log( edad );
+console.log( lugarNacimiento );
+
+//ejercicio: define las declaraciones para 5 datos característicos de un coche
+const marca = "Mercedes";
+var aniosDeUso = 5;
+const paísDeCompra = "México";
+const color = "rojo";
+const velocidadMáxima = "200 km/h";
+
+console.log( marca );
+console.log( aniosDeUso );
+console.log( paísDeCompra );
+console.log( color );
+console.log( velocidadMáxima );
+
+//tipos de datos
+//Tipo de dato número
+var número1 = 5;
+var número2 = 1500;
+
+//Tipo de dato Texto
+var texto1 = "Bola"
+var texto2 = "Bienvenido al curso"
+
+//Tipo de dato Booleano
+var booleanoVerdadero = true;
+var booleanoFalso = false;
+
+//Tipo de dato Null
+var condición = null;
+
+//Tipo de dato Undefined
+var valorIndefinido;
+console.log ( valorIndefinido ); 
+
+//Tipo de dato Arreglo
+const arregloNumeros = [1,2,3];
+const arregloNumeros = ["Luis","Erik","Ana"];
+const arregloMixto = ["Luis",2,"Ana"];
+
+//Tipo de dato Objeto
+var auto {marca: "Toyota", modelo:"Corolla", año: 2010};
+
+//ejercicio: objeto persona
+var persona = {
+    nombre: "Diego", 
+    apellido paterno: "Perez",
+    appelido materno = "Dominguez",
+    edad: "16",
+    Lugar de Nacimiento "México",
+    }
